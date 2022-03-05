f0480662a9f3c5ebec9c698ff65ab19fceda2165
Filipe Dias <filipediascontato@gmail.com>
2022-03-05 10:21:00 -0300

    Fixes header styles

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -3,9 +3,10 @@ import styled from "styled-components";
 const Button = styled.button`
   padding: 0 1.2rem;
   margin-left: 2.4rem;
+  min-height: 3rem;
 
   font: 500 1.4rem "Inter", sans-serif;
-  font-stretch: 100%;
+  line-height: 1.25rem;
 
   background: ${props => props.theme.colors.white};
   border: 0.1rem solid ${props => props.theme.colors.white};
@@ -74,8 +75,11 @@ const StyledHeader = styled.header`
   display: flex;
   align-items: center;
 
-  margin: 2.35rem 15.55rem;
+  margin: auto;
   padding: 0 2.4rem;
+  min-height: 8rem;
+  max-width: 104.8rem;
+  width: 90%;
 
   font: 1.4rem "Inter", sans-serif;
   color: ${props => props.theme.colors.grey};
@@ -84,6 +88,7 @@ const StyledHeader = styled.header`
 const Right = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
 `;
 
 export { Button, Item, List, Left, Span, StyledHeader, Right };
