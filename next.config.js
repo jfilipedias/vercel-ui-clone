ea12547c5fa12fb1fe01bd625c072deab656ebf0
Filipe Dias <filipediascontato@gmail.com>
2022-03-04 22:24:19 -0300

    Adds next-images component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withImages = require("next-images");
 
-module.exports = nextConfig;
+module.exports = withImages({ esModule: true });
